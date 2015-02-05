b1347f3dac67fc1015d56b1d4a6fc76d3550f663
Hack Reactor Students <accounts@hackreactor.com>
2015-02-04 16:25:36 -0800

    Finished implementation for psuedoclassical instatination technique

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.next = 0;
+  this.front = 0;
 };
+
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.next] = val;
+  this.next++;
+};
+
+Queue.prototype.dequeue = function() {
+  if( this.next !== this.front) {
+    var val = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return val;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.next - this.front;
+};
+
 
 
