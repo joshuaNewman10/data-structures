30068795457a56759157ba50e9f2d602644d44c1
Hack Reactor Students <accounts@hackreactor.com>
2015-02-04 20:46:15 -0800

    Finished Tree implementation and complexity analysis. Contains needs refactoring

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,23 +2,40 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
+  extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
-
+var extend = function( to, from ) {
+  for(var prop in from) {
+    to[prop] = from[prop];
+  }
+};
 
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value){ // O(1)
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target){ //recursive O(n) could be better though :(
+  var found = false;
+  var innerFunc = function(tree) {
+     if( tree.value === target) {
+      found = true;
+     } else {
+        for(var i=0; i<tree.children.length; i++) {
+          innerFunc(tree.children[i]);
+        }
+     }
+  };
+  innerFunc(this);
+  return found;
 
 };
 
