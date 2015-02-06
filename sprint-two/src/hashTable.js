9ec6881d167cd0724bf81570da21e9c3e6275f7b
Hack Reactor Students <accounts@hackreactor.com>
2015-02-05 17:56:00 -0800

    set and hashtables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,18 +4,44 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if ( bucket === undefined ){
+    this._storage.set(index, [[k, v]]);
+    return;
+  } else {
+    for(var i=0; i<bucket.length; i++) {
+      if( bucket[i][0] === k ) {
+        bucket[i][1] = v;
+        return;
+      }
+    }
+  }
+  bucket.push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for(var i=0; i<bucket.length; i++) {
+    if( bucket[i][0] === k ) {
+      return bucket[i][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var val;
+  for(var i=0; i<bucket.length; i++) {
+    if( bucket[i][0] === k) {
+      val = bucket[i][1];
+      bucket.splice(i, 1);
+    }
+  }
 };
-
 
 
 /*
